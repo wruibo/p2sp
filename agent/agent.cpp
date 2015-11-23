7f4fd6b7d471b02bef9c1ef7f4d074716a6d81e9
wruibo <wruibo@gmail.com>
2015-11-23 16:56:26 +0800

    for backup

--- agent/agent.cpp
+++ agent/agent.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 	cout<<"in agent"<<endl;
+	void** pvoid = new void*[10];
+	int a = 1;
+	pvoid[0] = &a;
+	cout<<"value :"<<*(int*)pvoid[0]<<endl;
 	return 0;
 }
 
