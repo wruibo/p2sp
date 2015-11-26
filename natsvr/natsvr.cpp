331461f8fbcd2061e02ac99ff8c0267b5eb10eca
wruibo <wruibo@gmail.com>
2015-11-26 10:56:27 +0800

    add new files

--- natsvr/natsvr.cpp
+++ natsvr/natsvr.cpp
@@ -4,7 +4,19 @@
  *  Created on: 2015年10月20日
  *      Author: wrb00_000
  */
+#include "cube/service/udp/linux/server.h"
 
+class myhandler: public cube::service::handler{
+public:
+	int handle(const cube::service::dpacket_t *req, cube::service::dpacket_t **resp, void *arg){
+		return 0;
+	}
+};
 
+int main(int argc, char* argv[]) {
+	cube::service::server<myhandler> svc;
+	svc.start(8000, 1, 0);
 
+	return 0;
+}
 
---
+++ tools/cubet.cpp
@@ -0,0 +1,13 @@
+/*
+ * cubet.cpp
+ *
+ *  Created on: 2015年11月25日
+ *      Author: wrb00_000
+ */
+
+int main(int argc, char * argv[]){
+	return 0;
+}
+
+
+
